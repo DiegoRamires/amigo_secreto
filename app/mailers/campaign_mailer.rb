c15a3d4c2f7d79019a6973f8b7a118bb78a974e4
Diego Ramires <goncalves.ramires@gmail.com>
2017-12-20 17:45:38 -0200

    Change email layout, configure raffle.html to inky-rb gem

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -1,9 +1,9 @@
 class CampaignMailer < ApplicationMailer
   
-    def raffle(campaign, member, friend)
-      @campaign = campaign
-      @member = member
-      @friend = friend
-      mail to: @member.email, subject: "Nosso Amigo Secreto: #{@campaign.title}"
-    end
+  def raffle(campaign, member, friend)
+    @campaign = campaign
+    @member = member
+    @friend = friend
+    mail to: @member.email, subject: "Nosso Amigo Secreto: #{@campaign.title}"
   end
+end
