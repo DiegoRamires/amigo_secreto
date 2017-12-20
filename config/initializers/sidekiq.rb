6a7b21e740fd8abf633b32ee147ac4e1fe64307e
Diego Ramires <goncalves.ramires@gmail.com>
2017-12-20 17:33:28 -0200

    Adjust mailcatcher configuration to receive campaign emails

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,5 +3,5 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://redis:6379/0'}
+  config.redis = { url: 'redis://redis:6379/0' }
 end
